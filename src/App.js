e16134da1ae53b12a3048f8328b78ac8f22d97a8
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-08 15:34:27 -0500

    figuring how to process iterated fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import React, { Component } from 'react';
 import './App.css';
+import Square from './Square.js';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+function getAllPhotos(photosArray){
+  let tempArray = [];
+  for (let i = 0; i < photosArray.length; i ++) {
+    console.log(photosArray[i])
+    tempArray.push(<Square pic = {photosArray[i]}/>)
+  }
+  return tempArray;
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    };
+  }
+
+  componentDidMount(){
+    const url = 'https://picsum.photos/v2/list?limit=15';
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data)
+        this.setState({photos:data})
+      })
+  }
+
+  render () {
+    //console.log(this.state.photos[0])
+    let allPhotos = "";
+
+    if (this.state.photos.length) {
+      allPhotos=getAllPhotos(this.state.photos)
+    }
+
+console.log(allPhotos)
+    return (
+      <div>
+        <h1>alright alright alriiiight</h1>
+        {allPhotos}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Square.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import App from './App';
+
+function Square(props) {
+  console.log(props)
+  return(
+    <div className='square'>
+      <h6>this will be a square {''}</h6>
+    </div>
+  )
+}
+
+
+
+
+
+
+export default Square;
