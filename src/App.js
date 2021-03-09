d9fa96428b783eebb311fcae4ef08360f3ddc203
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-08 21:03:42 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -3,11 +3,36 @@ import './App.css';
 import Square from './Square.js';
 
 
-function getAllPhotos(photosArray){
+
+let squares = document.querySelectorAll('.square');
+console.log(squares)
+// let modal = document.querySelector('.modal');
+// let modalClose = document.querySelector('.close');
+// let modTop = document.querySelector('.modTop');
+// let modBtm = document.querySelector('.modBtm');
+
+// modalClose.addEventListener('click', function () {
+//   modal.style.display = 'none';
+// })
+
+// for (let j = 0; j < squares.length; j++) {
+//   squares[j].addEventListener('click', function () {
+//     //console.log(this.style)
+//     modal.style.display = 'block';
+//     modTop.style.background = this.style.background
+//     modTop.style.backgroundSize = 'cover'
+//     modBtm.innerText = "download me"
+//   })
+// }
+
+function getAllPhotos(photosArray) {
   let tempArray = [];
-  for (let i = 0; i < photosArray.length; i ++) {
-    console.log(photosArray[i])
-    tempArray.push(<Square pic = {photosArray[i]}/>)
+  for (let i = 0; i < photosArray.length; i++) {
+    //console.log(photosArray[i].download_url)
+    let add = photosArray[i].download_url
+    tempArray.push(<Square pic={photosArray[i]} />)
+    //squares[i].style.background = `url(${add})`
+    //console.log(squares[i].style)
   }
   return tempArray;
 }
@@ -21,31 +46,34 @@ class App extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const url = 'https://picsum.photos/v2/list?limit=15';
 
     fetch(url)
       .then(response => response.json())
       .then(data => {
         //console.log(data)
-        this.setState({photos:data})
+        this.setState({ photos: data })
       })
   }
 
-  render () {
+  render() {
     //console.log(this.state.photos[0])
     let allPhotos = "";
 
     if (this.state.photos.length) {
-      allPhotos=getAllPhotos(this.state.photos)
+      allPhotos = getAllPhotos(this.state.photos)
     }
 
-console.log(allPhotos)
+    //console.log(allPhotos)
     return (
-      <div>
-        <h1>alright alright alriiiight</h1>
-        {allPhotos}
-      </div>
+      <body>
+        <h1>Alright Alright Alriiiight</h1>
+        <div className='section'>
+          {allPhotos}
+        </div>
+        {/* <Square pic = {this.state}/> */}
+      </body>
     )
   }
 }
--- src/Square.js
+++ src/Square.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import App from './App';
+import './Square.css';
 
 function Square(props) {
-  console.log(props)
+  //console.log(props.pic)
   return(
     <div className='square'>
       <h6>this will be a square {''}</h6>
